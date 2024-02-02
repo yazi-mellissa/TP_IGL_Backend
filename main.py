ac14859aaa8f165c79afc872b408680b2500ad3c
Raoul <la_merhab@esi.dz>
2024-02-02 01:03:57 +0100

    CORS pour le navigateur

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import uvicorn
 from fastapi import FastAPI
 from routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router=router)
 
 @app.get("/")
